e3cfecae23e04d8f064c9bb24b839b2c9574cc32
Fredrick Mwangi <berrymundia@gmail.com>
2026-02-10 18:09:21 +0300

    Add NationalId to UpdatePatientRequest and map it in UpdatePatient API
    - Fixed null constraint error when updating patients
    - Ensures NationalId is included in the UpdatePatientCommand
    - Maps all required fields from API request to command

---
+++ LifeCare.API/Controllers/Requests/UpdatePatientRequest.cs
@@ -0,0 +1,20 @@
+namespace LifeCare.API.Controllers.Requests;
+ 
+public class UpdatePatientRequest
+{
+    public string NationalId { get; set; }  
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Gender { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    
+    public string State { get; set; }
+    public string ZipCode { get; set; }
+    public string? GuardianName { get; set; }
+    public string? GuardianRelationship { get; set; }
+    public string? GuardianPhone { get; set; }
+}
